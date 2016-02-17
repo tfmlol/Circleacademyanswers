539cb1f4f8752052fabbd5d4c1e5166ed3e42bb1
tfmlol <tfm2lol@gmail.com>
2016-02-16 18:20:22 -0600

    Answers

---
+++ 1.js
@@ -0,0 +1,5 @@
+var myArray = [1,2,3,4];
+myArray.push(5);
+console.log(myArray)
+
+aler("myArray");
---
+++ 2.js
@@ -0,0 +1,20 @@
+var FantasyTeams = [
+  "Immortals",
+  "Echo Fox",
+  "TSM",
+  "Team Liquid",
+  "Cloud 9",
+  "Dignitas"
+]
+
+FantasyTeams.map(function(x) {
+  console.log(x);
+})
+
+//console.log("hello world!")
+
+
+
+/*FantasyTeams.map(function(Team){
+  console.log(team);
+})
